a1100ff2c14cd5164729f90114bd7999425519a7
Sergio Cabral <sergio@cabral.br.com>
2018-12-01 00:57:09 -0200

    Traduções.

--- 1-DSV/skript/src/Luckygames/WebSocketControl.tsx
+++ 1-DSV/skript/src/Luckygames/WebSocketControl.tsx
@@ -21,8 +21,8 @@ namespace Skript.Luckygames {
          * @returns {Promise<boolean>} Retorna true quando há sucesso.
          */
         public static initialize(tentatives: number = 0): Promise<boolean> {
-            if (this.initialized !== undefined) throw new Core.Errors.InvalidCommand("this.initialize() more than 1x");
-            this.initialized = false;
+            if (WebSocketControl.initialized !== undefined) throw new Core.Errors.InvalidCommand("WebSocketControl.initialize() more than 1x");
+            WebSocketControl.initialized = false;
 
             new WebSocketControlBus(this);
             
@@ -30,7 +30,7 @@ namespace Skript.Luckygames {
                 const check = (tentativesCount: number) => {
                     skript.log.post("Trying to intercept WebSocket.", null, Core.Log.Level.DebugLuckygames);
 
-                    const ws = this.websocket();
+                    const ws = WebSocketControl.websocket();
                     if (tentativesCount > 0 && !ws) setTimeout(() => check(--tentativesCount), 1000);
                     else {
                         if (ws && ws.constructor.name !== WebSocket.name) throw new Core.Errors.InvalidArgument(`Luckygames.io WebSocket type: ${ws.constructor.name} == ${WebSocket.name}`);
@@ -38,13 +38,13 @@ namespace Skript.Luckygames {
                         const result = !!ws;
 
                         if (result) {
-                            this.currentMode = WebSocketMode.Normal;
+                            WebSocketControl.currentMode = WebSocketMode.Normal;
                             skript.log.post("WebSocket successfully intercepted.", null, Core.Log.Level.DebugLuckygames, ws);
-                            this.initialized = true;
+                            WebSocketControl.initialized = true;
                         }
                         else {
-                            this.currentMode = undefined;
-                            skript.log.post("Could not adjust WebSockets operation of luckygames.io after {0} tentatives.", tentatives, Core.Log.Level.Error);
+                            WebSocketControl.currentMode = undefined;
+                            skript.log.post("After {0] attempts it was not possible to set up global information for luckygames.io. But do not worry. This is not a serious problem.", tentatives, Core.Log.Level.Error);
                         }
 
                         resolve(result);
@@ -61,7 +61,7 @@ namespace Skript.Luckygames {
         private static websocket(): WebSocket|undefined {
             const socket = (window as any).socket;
             const ws = socket && socket.id ? socket.id : undefined;
-            if (socket && ws) this.intercept(socket);
+            if (socket && ws) WebSocketControl.intercept(socket);
             return ws;
         }
                 
@@ -71,8 +71,8 @@ namespace Skript.Luckygames {
          */
         private static intercept(socket: any): void {
             if (!socket || socket.intercepted) return;
-            this.socketReconnectBackup = socket.Reconnect;
-            socket.Reconnect = this.socketReconnectNew;
+            WebSocketControl.socketReconnectBackup = socket.Reconnect;
+            socket.Reconnect = WebSocketControl.socketReconnectNew;
             socket.intercepted = true;
         }
 
@@ -86,13 +86,13 @@ namespace Skript.Luckygames {
          * @param {any} param A análise de parâmetros determina se a chamada é externa ou interna.
          */
         private static socketReconnectNew(param?: any): any {
-            if (!this.socketReconnectBackup) throw new Core.Errors.NullNotExpected("this.socketReconnectBackup");
+            if (!WebSocketControl.socketReconnectBackup) throw new Core.Errors.NullNotExpected("WebSocketControl.socketReconnectBackup");
 
-            const mode = this.mode()            
+            const mode = WebSocketControl.mode()            
             switch (mode) {
                 case WebSocketMode.Normal: 
                     skript.log.post("Request to reconnect WebSocket was accepted. Current interceptor mode: {0}", WebSocketMode[mode], Core.Log.Level.DebugLuckygames, param);
-                    return this.socketReconnectBackup();
+                    return WebSocketControl.socketReconnectBackup();
                 case WebSocketMode.Off: 
                     skript.log.post("Request to reconnect WebSocket was ignored. Current interceptor mode: {0}", WebSocketMode[mode], Core.Log.Level.DebugLuckygames, param);
                     return;
@@ -100,7 +100,7 @@ namespace Skript.Luckygames {
                     const accept = param && param.mode === WebSocketMode[WebSocketMode.Reduce];
                     if (accept) {
                         skript.log.post("Request to reconnect WebSocket was accepted. Current interceptor mode: {0}", WebSocketMode[mode], Core.Log.Level.DebugLuckygames, param);
-                        return this.socketReconnectBackup();
+                        return WebSocketControl.socketReconnectBackup();
                     } else {
                         skript.log.post("Request to reconnect WebSocket was ignored. Current interceptor mode: {0}", WebSocketMode[mode], Core.Log.Level.DebugLuckygames, param);
                         return;
@@ -113,7 +113,7 @@ namespace Skript.Luckygames {
          * Finaliza a conexão do WebSocket.
          */
         private static socketClose(): void {
-            const ws = this.websocket();
+            const ws = WebSocketControl.websocket();
             if (!ws) return;
             if (ws.readyState != WebSocket.CLOSED) {
                 ws.close();
@@ -157,47 +157,48 @@ namespace Skript.Luckygames {
          * Executa as ações para o modo WebSocketMode.Reduce.
          */
         private static executeModeReduce(): void {
-            this.socketClose();
-            this.idTimeoutReduce = setTimeout(() => {
-                this.socketReconnectNew({ mode: WebSocketMode[WebSocketMode.Reduce] });
-                this.idTimeoutReduce = setTimeout(() => {
-                    this.executeModeReduce();
-                }, this.intervalTimeoutReduce.on);
-            }, this.intervalTimeoutReduce.off);
+            WebSocketControl.socketClose();
+            WebSocketControl.idTimeoutReduce = setTimeout(() => {
+                WebSocketControl.socketReconnectNew({ mode: WebSocketMode[WebSocketMode.Reduce] });
+                WebSocketControl.idTimeoutReduce = setTimeout(() => {
+                    WebSocketControl.executeModeReduce();
+                }, WebSocketControl.intervalTimeoutReduce.on);
+            }, WebSocketControl.intervalTimeoutReduce.off);
         }
 
         /**
-         * Modo atual do funcionamento do this.
+         * Modo atual do funcionamento do WebSocketControl.
          * @returns {WebSocketMode} Estado atual.
          */
         public static mode(mode?: WebSocketMode): WebSocketMode {
-            if (this.currentMode === undefined || !this.initialized) {
-                if (mode !== undefined) skript.log.post("Could not adjust WebSockets operation of luckygames.io.", null, Core.Log.Level.Error);
+            if (WebSocketControl.currentMode === undefined || !WebSocketControl.initialized) {
+                if (mode !== undefined) skript.log.post("Failed to set up receiving global information from luckygames.io. But do not worry. This is not a serious problem.", null, Core.Log.Level.Error);
                 return WebSocketMode.Normal;
             }
 
-            if (mode !== undefined && mode !== this.currentMode) {
-                if (this.idTimeoutReduce) clearTimeout(this.idTimeoutReduce);
+            if (mode !== undefined && mode !== WebSocketControl.currentMode) {
+                if (WebSocketControl.idTimeoutReduce) clearTimeout(WebSocketControl.idTimeoutReduce);
 
                 skript.log.post("Change WebSocket mode to: {0}", WebSocketMode[mode], Core.Log.Level.DebugLuckygames);
 
-                const ws = this.websocket();
-                if (ws) switch (this.currentMode = mode) {
+                const ws = WebSocketControl.websocket();
+                if (ws) switch (WebSocketControl.currentMode = mode) {
                     case WebSocketMode.Normal:
-                        if (ws.readyState != WebSocket.OPEN) this.socketReconnectNew();
+                        if (ws.readyState != WebSocket.OPEN) WebSocketControl.socketReconnectNew();
                         break;
                     case WebSocketMode.Off: 
-                        this.socketClose();
+                        WebSocketControl.socketClose();
                         break;
                     case WebSocketMode.Reduce:                         
-                        this.executeModeReduce();                        
+                        WebSocketControl.executeModeReduce();                        
                         break;
                     default: throw new Core.Errors.InvalidArgument(`WebSocketMode = ${mode}`);
                 }
+                skript.log.post("Defined as \"{0}\" receiving global information from luckygames.io.", skript.translate.get(WebSocketMode[WebSocketControl.currentMode]));
                 new Message.WebSocketModeWasChanged(mode).sendAsync();
             }
 
-            return this.currentMode;
+            return WebSocketControl.currentMode;
         }
     }
 }
