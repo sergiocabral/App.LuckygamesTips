4c6475b2d67ddf909d84edcf15184c71b130fb2b
Sergio Cabral <sergio@cabral.br.com>
2018-12-03 04:15:53 -0200

    AceEditor início

--- 1-DSV/skript/src/Part/System/Parameters/Parameters.tsx
+++ 1-DSV/skript/src/Part/System/Parameters/Parameters.tsx
@@ -10,6 +10,12 @@ namespace Skript.Part.System.MainHeader {
          */
         protected stylesheet: string = `
             ${this.selector()} {
+            }            
+            ${this.selector()} .json > div {
+                height: 180px;
+            }
+            ${this.selector()} .json.edicao .ace_editor .ace_gutter {
+                background-color: brown;
             }
         `;
 
@@ -22,8 +28,54 @@ namespace Skript.Part.System.MainHeader {
             
             this.title = this.translate("Parameters to initialization");
             this.icon = "fas fa-users-cog";
+
+            this.elAceEditorJson = React.createRef();
         }
         
+        /**
+         * Id para componente AceEditor para o campo json.
+         * @type {string}
+         */
+        private idAceEditorJson: string = Util.Text.random();
+
+        /**
+         * Elemento container do AceEditor para o campo json.
+         * @type {React.RefObject<HTMLDivElement>}
+         */
+        private elAceEditorJson: React.RefObject<HTMLDivElement>;
+
+        /**
+         * Referência para o AceEditor para o campo json.
+         * @type {any}
+         */
+        private objAceEditorJson?: any;
+
+        /**
+         * Espaçamento do TAB na formatação do json.
+         * @type {number}
+         */
+        private jsonTabSize: number = 2;
+
+        /**
+         * Parâmetros atuais como json
+         * @returns {string} json
+         */
+        public json(): string {
+            return JSON.stringify({ teste: 123, ops: "hahaha" }, null, this.jsonTabSize);
+        }
+
+        /**
+         * Define um conteúdo json no AceEditor.
+         * @param json json
+         */
+        private setJson(json: string): void {
+            if (!this.objAceEditorJson || !this.elAceEditorJson.current || !this.elAceEditorJson.current.parentElement) return;
+            this.objAceEditorJson.setValue(json);
+            this.objAceEditorJson.selection.clearSelection();
+            this.objAceEditorJson.selection.moveCursorFileStart();
+            this.elAceEditorJson.current.parentElement.classList.remove('edicao');
+        }
+
         /**
          * Renderizador do React.
          * @returns {JSX.Element}
@@ -31,9 +83,28 @@ namespace Skript.Part.System.MainHeader {
         protected renderContent(): JSX.Element {            
             return (
                 <div id={this.id()} className={this.className()}>
-                    Parameters    
+                    <div className="json"><div id={this.idAceEditorJson} ref={this.elAceEditorJson}></div></div>
                 </div>
             );
+        }
+
+        /**
+         * Após montagem do componente.
+         */
+        public componentDidMount(): void {
+            if (!this.elAceEditorJson.current) return;
+
+            this.objAceEditorJson = (window as any).ace.edit(this.idAceEditorJson);
+            this.objAceEditorJson.setTheme("ace/theme/twilight");
+            this.objAceEditorJson.session.setMode("ace/mode/json");
+            this.objAceEditorJson.session.setTabSize(this.jsonTabSize);
+            this.objAceEditorJson.session.setUseWrapMode(true);
+            this.objAceEditorJson.on('change', () => {
+                if (!this.elAceEditorJson.current || !this.elAceEditorJson.current.parentElement) return; 
+                if (!this.elAceEditorJson.current.parentElement.classList.contains('edicao')) 
+                    this.elAceEditorJson.current.parentElement.classList.add('edicao'); });
+
+            this.setJson(this.json());
         }
     }
 
